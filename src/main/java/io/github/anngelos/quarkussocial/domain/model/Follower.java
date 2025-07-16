e97eaff2118012de258f5f9366a9e6419e9cc5ce
Angelo Cardoso <hoanngelos@gmail.com>
2025-07-16 17:31:11 -0300

    Follower class updated

--- src/main/java/io/github/anngelos/quarkussocial/domain/model/Follower.java
+++ src/main/java/io/github/anngelos/quarkussocial/domain/model/Follower.java
@@ -1,13 +1,21 @@
 package io.github.anngelos.quarkussocial.domain.model;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 @Entity
 @Table(name = "followers")
+@Data
 public class Follower {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
     private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "")
     private User follower;
 }
