06e5c30f38c40160bc36b47e2606ac2e0aa27b84
Angelo Cardoso <hoanngelos@gmail.com>
2025-07-22 00:38:41 -0300

    add more tests to PostResource

--- src/test/java/io/github/anngelos/quarkussocial/rest/PostResourceTest.java
+++ src/test/java/io/github/anngelos/quarkussocial/rest/PostResourceTest.java
@@ -8,6 +8,7 @@ import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
 import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -39,10 +40,57 @@ class PostResourceTest {
     var post = new CreatePostRequest();
     post.setText("Anything!");
 
-    var userId = 1;
-
     given().contentType(ContentType.JSON).body(post).pathParam("userId", userId)
             .when().post()
             .then().statusCode(201);
   }
+
+  @Test
+  @DisplayName("should return 404 when trying to make a post for an inexistent user")
+  public void postForAnInexistentUserTest() {
+    var post = new CreatePostRequest();
+    post.setText("Anything!");
+
+    var inexistentUserId = 999;
+
+    given().contentType(ContentType.JSON).body(post).pathParam("userId", inexistentUserId)
+            .when().post()
+            .then().statusCode(404);
+  }
+
+  @Test
+  @DisplayName("should return 404 when user doesn't exist")
+  public void listPostUserNotFoundTest() {
+    var inexistentUserId = 999;
+    given().contentType(ContentType.JSON).pathParam("userId", inexistentUserId)
+            .when().get()
+            .then().statusCode(404);
+  }
+
+  @Test
+  @DisplayName("should return 400 when followerId header is not present")
+  public void listPostFollowerHeaderNotSentTest() {
+    given().pathParam("userId", userId)
+            .when().get()
+            .then().statusCode(400)
+            .body(Matchers.is("You forgot the header: followerId."));
+  }
+
+  @Test
+  @DisplayName("should return 400 when follower doesn't exist")
+  public void listPostFollowerHeaderNotFoundTest() {
+    var inexistentFollowerId = 999;
+    given().pathParam("userId", userId).header("followerId", inexistentFollowerId)
+            .when().get()
+            .then().statusCode(400)
+            .body(Matchers.is("Inexistent followerId."));
+  }
+
+  @Test
+  @DisplayName("should return 403 when follower isn't follower")
+  public void listPostNotAFollowerHeaderNotFoundTest() {}
+
+  @Test
+  @DisplayName("should return posts")
+  public void listPostTest() {}
 }
